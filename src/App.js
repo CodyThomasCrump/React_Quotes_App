0451a21e0c84dc31b0273ae8a3e2fecf24c51978
CodyThomasCrump <codytcrump@gmail.com>
2019-11-04 14:32:06 -0600

    Added the function to generate a random color. Set up the QuoteBox component with some styling

--- src/App.js
+++ src/App.js
@@ -34,10 +34,21 @@ class App extends Component {
     });
   }
 
+  //Function to generate a random color from R: 0-154, G: 0-154, B: 0-154
+  getRandomColor(upperBound) {
+    const color = `rgb(
+      ${Math.floor(Math.random() * upperBound)},
+      ${Math.floor(Math.random() * upperBound)},
+      ${Math.floor(Math.random() * upperBound)}
+    )`;
+    return color;
+  }
+
   render() {
     return (
       <div>
         <QuoteBox
+        color={this.getRandomColor(155)}
           quote={this.state.quote}
           author={this.state.author}
           handleClick={this.handleClick}
