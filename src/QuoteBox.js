57b624637ff37421765dd4c5d453ad43c32faa24
CodyThomasCrump <codytcrump@gmail.com>
2019-11-06 10:38:32 -0600

    Worked on the css and styled the qoute boxes. changed the font as well

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import './QuoteBox.css';
 
 class QuoteBox extends Component {
     render() {
 
         //Set the background color of the HTML element to the random color that is passed in as a prop
         const html = document.documentElement;
-        html.style.backgroundColor = this.props.color;
+        html.style.background = this.props.color;
+
         return (
-            <div style={{ backgroundColor: 'white' }}>
-                <h1 style={{ color: this.props.color }}>{this.props.quote}</h1>
-                <h4 style={{ color: this.props.color}}>
-                    - {this.props.author !== '' ? this.props.author : 'Unknown'} -
-                </h4>
-                <button onClick={this.props.handleClick}>New Quote</button>
+            <div style={{ backgroundColor: 'white' }} className='QuoteBox'>
+                <h1 className='QuoteBox-quote' style={{ color: this.props.color }}>{this.props.quote}</h1>
+                <h4 className='QuoteBox-author' style={{ color: this.props.color}}>
+                    - {this.props.author !== '' ? this.props.author : 'Unknown'} - </h4>
+                <button className='QuoteBox-button' style={{backgroundColor: this.props.color}} onClick={this.props.handleClick}>New Quote</button>
             </div>
         );
     }
